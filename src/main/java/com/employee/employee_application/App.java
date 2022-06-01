59a5e410b3658d21a12036885c124773ac3f02c9
harishdasari66 <harishdasari66@gmail.com>
2022-06-01 15:43:17 +0530

    initial commit

---
+++ src/main/java/com/employee/employee_application/App.java
@@ -0,0 +1,31 @@
+package com.employee.employee_application;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.maven.dto.Employee;
+import com.maven.service.EmployeeServiceImpl;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+       
+    	
+    	EmployeeServiceImpl employeeService =new EmployeeServiceImpl();
+    	
+    	
+    	Employee employee =new Employee(101, "rohit", 10000.0, "JAVA");
+    	
+     Optional<Employee> addedEmployee =	employeeService.addEmployee(employee);
+    	
+     if(addedEmployee.isPresent())
+     {
+    	 System.out.println("employee added sucessfull");
+     }
+     
+     List<Employee> employees = employeeService.getEmployees();
+     System.out.println(employees);
+    	
+    }
+}
